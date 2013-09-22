e254d621a3efa9f4568e61f12b8d7525a1a50987
Doug Ellison <doug.ellison@gmail.com>
2013-09-22 10:51:49 -0600

    Setting up a new Repo for the CS5800 Class

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('mainApp', [])
+	.controller('main', function($scope) {
+		$scope.hello = "Hello World";
+	})
+
+;
+  
